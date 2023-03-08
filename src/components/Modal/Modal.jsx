bc9c28e2af4d184e524b005c437fd09357c2dba5
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-08 13:38:03 +0200

    Refactoring/ class Modal => function+hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,52 +1,83 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 import { Backdrop, Content, ButtonClose } from './Modal.styled';
-import { AiFillCloseCircle } from 'react-icons/ai';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscKeyDown);
-  }
+export function Modal({ onToggleModal, children }) {
+  useEffect(() => {
+    const handleEscKeyDown = e => {
+      if (e.code === 'Escape') onToggleModal();
+    };
+    window.addEventListener('keydown', handleEscKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleEscKeyDown);
+    };
+  }, [onToggleModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscKeyDown);
-  }
-
-  handleEscKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onToggleModal();
-    }
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) onToggleModal();
   };
 
-  handleBackdropClick = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onToggleModal();
-    }
-  };
+  const handleOnCloseButton = () => onToggleModal();
 
-  handleOnCloseButton = () => {
-    this.props.onToggleModal();
-  };
-
-  render() {
-    return createPortal(
-      <Backdrop onClick={this.handleBackdropClick}>
-        <Content>
-          <ButtonClose type="button" onClick={this.handleOnCloseButton}>
-            <AiFillCloseCircle />
-          </ButtonClose>
-          {this.props.children}
-        </Content>
-      </Backdrop>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <Backdrop onClick={handleBackdropClick}>
+      <Content>
+        <ButtonClose type="button" onClick={handleOnCloseButton}>
+          <AiFillCloseCircle />
+        </ButtonClose>
+        {children}
+      </Content>
+    </Backdrop>,
+    modalRoot
+  );
 }
 
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleEscKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleEscKeyDown);
+//   }
+
+//   handleEscKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onToggleModal();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onToggleModal();
+//     }
+//   };
+
+//   handleOnCloseButton = () => {
+//     this.props.onToggleModal();
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Backdrop onClick={this.handleBackdropClick}>
+//         <Content>
+//           <ButtonClose type="button" onClick={this.handleOnCloseButton}>
+//             <AiFillCloseCircle />
+//           </ButtonClose>
+//           {this.props.children}
+//         </Content>
+//       </Backdrop>,
+//       modalRoot
+//     );
+//   }
+// }
+
 Modal.propTypes = {
-  onClose: PropTypes.func.isRequired,
+  onToggleModal: PropTypes.func.isRequired,
+  children: PropTypes.node,
 };
